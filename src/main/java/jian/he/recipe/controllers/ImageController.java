0a277b9bfc320b894e59001198e09815c07b051e
jianheMark <jianhe.mark@pm.me>
2021-08-26 15:14:49 +0530

    updating.

--- src/main/java/jian/he/recipe/controllers/ImageController.java
+++ src/main/java/jian/he/recipe/controllers/ImageController.java
@@ -1,7 +1,9 @@
 package jian.he.recipe.controllers;
 
+import jian.he.recipe.commands.RecipeCommand;
 import jian.he.recipe.services.ImageService;
 import jian.he.recipe.services.RecipeService;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -32,7 +39,26 @@ public class ImageController {
                                   @RequestParam("imagefile") MultipartFile file){
 
         imageService.saveImageFile(Long.valueOf(id), file);
+        System.out.println("test");
 
         return "redirect:/recipe/" + id + "/show";
+    }
+
+    @GetMapping("recipe/{id}/recipeimage")
+    public void renderImageFromDB(@PathVariable String id,
+                                  HttpServletResponse response) throws IOException {
+
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(id));
+        byte[] byteArrray = new byte[recipeCommand.getImage().length];
+        int i = 0;
+
+        if(recipeCommand.getImage() != null){
+            for(Byte wrappedByte: recipeCommand.getImage()){
+                byteArrray[i++] = wrappedByte;
+            }
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArrray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
--- src/main/java/jian/he/recipe/services/ImageServiceImpl.java
+++ src/main/java/jian/he/recipe/services/ImageServiceImpl.java
@@ -1,13 +1,43 @@
 package jian.he.recipe.services;
 
+import jian.he.recipe.domain.Recipe;
+import jian.he.recipe.repositroies.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Service
 @Slf4j
 public class ImageServiceImpl implements ImageService{
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-            log.debug("Received a file.");
+        try{
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+            int i = 0;
+
+            for(byte b: file.getBytes()){
+                byteObjects[i++] = b;
+            }
+            recipe.setIamge(byteObjects);
+            recipeRepository.save(recipe);
+
+        }catch (IOException e){
+            log.error("Error Occurred",e);
+        }
+
+        log.debug("Received a file.");
     }
 }
